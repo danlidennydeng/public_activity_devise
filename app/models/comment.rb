46643b305f50f9f58c0613b04d40fb7c4a90bafc
danlideng <danlideng@yahoo.com>
2016-12-30 07:08:06 +0800

    tried recipient on comment model. did not work. rollback.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
 
   
-  include PublicActivity::Model
-  tracked owner: :commenter, recipient: :commentee
+  # include PublicActivity::Model
+  # tracked owner: :commenter, recipient: :commentee
 
-  belongs_to :commenter, :class_name => "User"
-  belongs_to :commentee, :class_name => "User"
+  # belongs_to :commenter, :class_name => "User"
+  # belongs_to :commentee, :class_name => "User"
 
   belongs_to :user
   belongs_to :micropost
